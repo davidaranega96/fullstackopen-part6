dd9e8949c7225acb55bca2bfb5ddbe85396278bd
daranega <daranega@optimusprice.ai>
2023-11-07 10:24:11 +0100

    finished feedback exercise

--- part1/part1d/part1d-exercise/src/App.jsx
+++ part1/part1d/part1d-exercise/src/App.jsx
@@ -1,55 +1,70 @@
 import { useState } from 'react'
 
-const Hist = (props) => {
-  console.log(props.allClicks)
-  if (props.allClicks.length == 0) {
+const StatisticLine = (prompt) => {
+  return (
+    <tr><td>{prompt.text}:</td><td>{prompt.value}</td></tr>
+  )
+}
+
+
+const Statistics = (prompt) => {
+  console.log(prompt)
+  const all = prompt.counter.good + prompt.counter.neutral + prompt.counter.bad
+  const average = (prompt.counter.good - prompt.counter.bad) / all
+  const positive = (prompt.counter.good / all) * 100
+  console.log(all)
+  if (all == 0) {
     return (
       <div>
-        the app is used by pressing the mouse buttons
-      </div>
+        <h1>Statistics</h1>
+        <div>No feedback given</div>
+    </div>
     )
   }
   return (
     <div>
-      buttons press history: {props.allClicks.join(', ')}
+      <h1>Statistics</h1>
+      <table>
+        <tbody>
+        <StatisticLine text='good' value={prompt.counter.good}/>
+        <StatisticLine text='bad' value={prompt.counter.bad}/>
+        <StatisticLine text='neutral' value={prompt.counter.neutral}/>
+        <StatisticLine text='all' value={all}/>
+        <StatisticLine text='average' value={average}/>
+        <StatisticLine text='positive' value={positive}/>
+        </tbody>
+      </table>
     </div>
   )
 }
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
+const FeedbackButton = (props) => {
+  return (
+    <button onClick={() => props.onClick({ value: props.value })}>
+      {props.value}
+    </button>
+  );
+};
+
 
 const App = () => {
-  const [clicks, setClicks] = useState({left: 0, right: 0})
-  const [allClicks, setAll] = useState([])
-  const [total, setTotal] = useState(0)
+  // save clicks of each button to its own state
+  const [counter, setCounter] = useState({
+    good: 0, neutral: 0, bad: 0
+  })
 
-  const handleLeftClick = () => {
-    const updatedLeft = clicks.left + 1
-    setClicks({ ...clicks, left: updatedLeft })
-    setAll(allClicks.concat('L'))
-    setTotal(updatedLeft + clicks.right)
-  }
-  
-
-  const handleRightClick = () => {
-    const updatedRight = clicks.right + 1
-    setClicks({ ...clicks, right: updatedRight })
-    setAll(allClicks.concat('R'))
-    setTotal(clicks.left + updatedRight)
+  const AddCounter = (value) => {
+    console.log("Adding to counter", value.value)
+    setCounter({...counter, [value.value]: counter[[value.value]]+1})
   }
 
   return (
     <div>
-      {clicks.left}
-      <Button handleClick={handleLeftClick} text='left'/>
-      <Button handleClick={handleRightClick} text='right'/>
-      {clicks.right}
-      <Hist allClicks={allClicks} />
-      <p>Total clicks: {total}</p>
+      <h1>Give feedback</h1>
+      <FeedbackButton value='good' onClick={AddCounter}/>
+      <FeedbackButton value='neutral' onClick={AddCounter}/>
+      <FeedbackButton value='bad' onClick={AddCounter}/>
+      <Statistics counter={counter}/>
     </div>
   )
 }
